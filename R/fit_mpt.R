fbfba61f4c820e846778dc7482362ff705a40ec7
mariusbarth <marius.barth@uni-koeln.de>
2018-07-05 12:36:34 +0200

    Added input validation to `fit_mpt()`
    - eqn file
    - columns present
    - NAs
    - non-integer values

--- R/fit_mpt.R
+++ R/fit_mpt.R
@@ -42,9 +42,44 @@ fit_mpt <- function(
     id <- "Subject"
   }
   
-  # remove extraneous colums ----
-  valid_cols <- c(id, condition, get_eqn_categories(model))
+  # check MPT file
+  mpt_model <- TreeBUGS::readEQN(model)
+  
+  if(!is.data.frame(mpt_model)) {
+    "I can't comprehend your .eqn file."
+  }
+
+  
+  # remove extraneous colums and check if all specified columns are present
+  # in data
+  freq_cols <- get_eqn_categories(model)
+  valid_cols <- c(id, condition, freq_cols)
+  check_cols <- valid_cols %in% colnames(data)
+  
+  if(!all(check_cols)) {
+    stop("Variable \"", paste(valid_cols[!check_cols], collapse = ", "), "\" not found in data.frame.")
+  }
+  
   data <- data[, valid_cols]
+  
+  
+  # Check NAs ----
+  nas_found <- unlist(lapply(X = data, FUN = anyNA))
+  
+  if(any(nas_found)) {
+    stop("Variable \"", paste(valid_cols[nas_found], collapse = ", "), "\" contains missing values.")
+  }
+  
+  # Check whether freqencies are integer ----
+  not_integer <- unlist(lapply(X = data[, freq_cols], FUN = function(x) {
+      any(as.integer(x)!=x)
+    }
+  ))
+  
+  if(any(not_integer)) {
+    stop("Variable \"", paste(freq_cols[not_integer], collapse = ", "), "\" contains non-integer values.")
+  }
+
   
   # Ensure that id and condition are factors, also drops unused levels
   # data[[id]] <- factor(data[[id]])
