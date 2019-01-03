884b602b5cd3ed7a4b17b793250770944b6d3b46
mariusbarth <marius.barth@uni-koeln.de>
2019-01-03 13:34:34 +0100

    changes to pass R CMD CHECK

--- R/write_results.R
+++ R/write_results.R
@@ -4,6 +4,7 @@
 #' 
 #' @param results An object of class multiverseMPT.
 #' @param path a path where to save the files (e.g., \code{"C:/results/modelX_dataY_"})
+#' @importFrom rlang .data
 #' @export
 write_results <- function(results, path = "MPTmultiverse_"
                           # what = c("est_group", "est_indiv", "est_rho", 
@@ -14,23 +15,23 @@ write_results <- function(results, path = "MPTmultiverse_"
   # TODO: allow to specify which columns should be unnested and exported
   #       => requires to work with expressions/tidyverse-issues
   
-  readr::write_csv(tidyr::unnest(results, est_group), 
+  readr::write_csv(tidyr::unnest(results, .data$est_group), 
                    path = paste0(path,"est_group.csv"))
-  readr::write_csv(tidyr::unnest(results, est_indiv), 
+  readr::write_csv(tidyr::unnest(results, .data$est_indiv), 
                    path = paste0(path,"est_indiv.csv"))
-  readr::write_csv(tidyr::unnest(results, est_rho), 
+  readr::write_csv(tidyr::unnest(results, .data$est_rho), 
                    paste0(path,"est_rho.csv"))
   
-  readr::write_csv(tidyr::unnest(results, test_between), 
+  readr::write_csv(tidyr::unnest(results, .data$test_between), 
                    paste0(path,"test_between.csv"))
   
-  readr::write_csv(tidyr::unnest(results, gof), 
+  readr::write_csv(tidyr::unnest(results, .data$gof), 
                    paste0(path,"gof.csv"))
-  readr::write_csv(tidyr::unnest(results, gof_indiv), 
+  readr::write_csv(tidyr::unnest(results, .data$gof_indiv), 
                    paste0(path,"gof_indiv.csv"))
-  readr::write_csv(tidyr::unnest(results, gof_group), 
+  readr::write_csv(tidyr::unnest(results, .data$gof_group), 
                    paste0(path,"gof_group.csv"))
   
-  readr::write_csv(tidyr::unnest(results, fungibility), 
+  readr::write_csv(tidyr::unnest(results, .data$fungibility), 
                    paste0(path,"fungibility.csv"))
 }
