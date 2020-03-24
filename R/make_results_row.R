5148955bb6858274de16d33725118d530b18d6d5
Henrik Singmann <singmann@gmail.com>
2020-03-24 10:18:28 +0100

    rebuilt documentation, removed help page for internal make_results_row function

--- R/make_results_row.R
+++ R/make_results_row.R
@@ -16,20 +16,20 @@
 #   gof_indiv = list(tibble())
 # )
 
-#' Create
-#'
-#' Internal function, creates container for results
-#'
-#' @param model       Character.
-#' @param dataset     Character.
-#' @param pooling     Character.
-#' @param package     Character.
-#' @param method      Character.
-#' @param data        A \code{data.frame}.
-#' @param id          Character.
+# Create
+#
+# Internal function, creates container for results
+#
+# @param model       Character.
+# @param dataset     Character.
+# @param pooling     Character.
+# @param package     Character.
+# @param method      Character.
+# @param data        A \code{data.frame}.
+# @param id          Character.
 
 #' @importFrom magrittr %>%
-#' @keywords internal
+# @keywords internal
 
 make_results_row <- function(
   model,
